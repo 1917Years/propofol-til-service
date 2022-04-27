cb512d1e638ed577b39ab4ee02c74a1f26e77ced
최영찬 <gks54410@gmail.com>
2022-04-27 19:36:36 +0900

    Feat: Cors, Web Security 설정 추가

---
+++ src/main/java/propofol/tilservice/api/common/config/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package propofol.tilservice.api.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import propofol.tilservice.api.common.filter.PreFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PreFilter preFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().cors();
+
+        http.authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .addFilterBefore(preFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.addAllowedOriginPattern("*"); // 모든 IP 응답 허용
+        config.addAllowedHeader("*"); // 모든 헤더에 응답을 허용
+        config.addAllowedMethod("*"); // 모든 post, get, delete, fetch 허용
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+}
