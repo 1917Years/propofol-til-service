ad8b87b733e85f924949572c92a7e1418e8aad5a
최영찬 <gks54410@gmail.com>
2022-04-27 21:39:48 +0900

    Feat: 게시글 생성, 수정, 삭제, 조회 기능 추가

---
+++ src/main/java/propofol/tilservice/api/controller/BoardController.java
@@ -0,0 +1,68 @@
+package propofol.tilservice.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import propofol.tilservice.api.controller.dto.BoardListResponseDto;
+import propofol.tilservice.api.controller.dto.BoardCreateRequestDto;
+import propofol.tilservice.api.controller.dto.BoardResponseDto;
+import propofol.tilservice.api.controller.dto.BoardUpdateRequestDto;
+import propofol.tilservice.domain.board.entity.Board;
+import propofol.tilservice.domain.board.service.BoardService;
+import propofol.tilservice.domain.board.service.dto.BoardDto;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/boards")
+public class BoardController {
+    private final BoardService boardService;
+    private final ModelMapper modelMapper;
+
+    @GetMapping
+    public BoardListResponseDto getPageBoards(@RequestParam Integer page){
+        BoardListResponseDto boardListResponseDto = new BoardListResponseDto();
+        Page<Board> pageBoards = boardService.getPageBoards(page);
+        boardListResponseDto.setTotalPageCount(pageBoards.getTotalPages());
+        boardListResponseDto.setTotalCount(pageBoards.getTotalElements());
+        pageBoards.forEach(board -> {
+            boardListResponseDto.getBoards().add(modelMapper.map(board, BoardResponseDto.class));
+        });
+        return boardListResponseDto;
+    }
+
+    @PostMapping
+    public String createBoard(@Validated  @RequestBody BoardCreateRequestDto requestDto){
+        BoardDto boardDto = modelMapper.map(requestDto, BoardDto.class);
+        return boardService.saveBoard(boardDto);
+    }
+
+    @PostMapping("/{boardId}")
+    public String updateBoard(@PathVariable Long boardId, @RequestBody BoardUpdateRequestDto requestDto){
+        return boardService.updateBoard(boardId, requestDto);
+    }
+
+    @GetMapping("/{boardId}")
+    public BoardResponseDto getBoardInfo(@PathVariable Long boardId){
+        Board board = boardService.getBoard(boardId);
+        return createBoardResponse(board);
+    }
+
+    private BoardResponseDto createBoardResponse(Board board) {
+        BoardResponseDto boardResponseDto = new BoardResponseDto();
+        boardResponseDto.setId(board.getId());
+        boardResponseDto.setTitle(board.getTitle());
+        boardResponseDto.setContent(board.getContent());
+        boardResponseDto.setOpen(board.getOpen());
+        boardResponseDto.setRecommend(board.getRecommend());
+        return boardResponseDto;
+    }
+
+    @DeleteMapping("/{boardId}")
+    public String deleteBoard(@PathVariable Long boardId){
+        return boardService.deleteBoard(boardId);
+    }
+}
---
+++ src/main/java/propofol/tilservice/domain/board/service/BoardService.java
@@ -0,0 +1,63 @@
+package propofol.tilservice.domain.board.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import propofol.tilservice.api.controller.dto.BoardUpdateRequestDto;
+import propofol.tilservice.domain.board.entity.Board;
+import propofol.tilservice.domain.board.repository.BoardRepository;
+import propofol.tilservice.domain.board.service.dto.BoardDto;
+import propofol.tilservice.domain.exception.NotFoundBoard;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public Page<Board> getPageBoards(Integer pageNumber){
+        PageRequest pageRequest =
+                PageRequest.of(pageNumber - 1, 10, Sort.by(Sort.Direction.DESC, "id"));
+        return boardRepository.findAll(pageRequest);
+    }
+
+    public String saveBoard(BoardDto boardDto){
+        Board board = createBoard(boardDto);
+        boardRepository.save(board);
+        return "ok";
+    }
+
+    public Board getBoard(Long boardId){
+        return boardRepository.findById(boardId).orElseThrow(() -> {
+            throw new NotFoundBoard("게시물을 찾을 수 없습니다.");
+        });
+    }
+
+    public String deleteBoard(Long boardId){
+        Board board = getBoard(boardId);
+        boardRepository.delete(board);
+        return "ok";
+    }
+
+    private Board createBoard(BoardDto boardDto) {
+        Board board = Board.createBoard()
+                .title(boardDto.getTitle())
+                .content(boardDto.getContent())
+                .open(boardDto.getOpen())
+                .recommend(0)
+                .build();
+        return board;
+    }
+
+    @Transactional
+    public String updateBoard(Long boardId, BoardUpdateRequestDto requestDto) {
+        Board findBoard = getBoard(boardId);
+        findBoard.updateBoard(requestDto.getTitle(), requestDto.getContent(), requestDto.getOpen());
+        return "ok";
+    }
+}
