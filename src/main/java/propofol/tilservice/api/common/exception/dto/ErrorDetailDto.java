687f14f920122c11f51a8cb8a8bbe58d1f97a288
최영찬 <gks54410@gmail.com>
2022-04-27 21:43:15 +0900

    Feat: Error 응답 Dto 추가

---
+++ src/main/java/propofol/tilservice/api/common/exception/dto/ErrorDetailDto.java
@@ -0,0 +1,11 @@
+package propofol.tilservice.api.common.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorDetailDto {
+    private String field;
+    private String errorMessage;
+}
