5a721070d950466bb911408ce9cb225424eed4d7
최영찬 <gks54410@gmail.com>
2022-04-27 19:04:01 +0900

    Initial Commit

---
+++ src/main/java/propofol/tilservice/TilServiceApplication.java
@@ -0,0 +1,13 @@
+package propofol.tilservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilServiceApplication.class, args);
+	}
+
+}
