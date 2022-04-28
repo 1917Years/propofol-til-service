978dc356a7284ab3a72684d002df105bee7185ef
최영찬 <gks54410@gmail.com>
2022-04-28 22:33:14 +0900

    Feat: FeignClient 및 Eureka 활성화

--- src/main/java/propofol/tilservice/TilServiceApplication.java
+++ src/main/java/propofol/tilservice/TilServiceApplication.java
@@ -4,13 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients(basePackages = "propofol.tilservice.api.feign")
 @ConfigurationPropertiesScan(basePackages = "propofol.tilservice.api.common.properties")
+@EnableEurekaClient
 public class TilServiceApplication {
 
 	public static void main(String[] args) {
