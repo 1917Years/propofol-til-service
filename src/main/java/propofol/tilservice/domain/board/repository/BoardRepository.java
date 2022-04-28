3ad7b21db491d1648a6964af61989b6b86d823f8
최영찬 <gks54410@gmail.com>
2022-04-28 22:31:09 +0900

    Feat: 생성자를 통한 페이지 조회 추가

--- src/main/java/propofol/tilservice/domain/board/repository/BoardRepository.java
+++ src/main/java/propofol/tilservice/domain/board/repository/BoardRepository.java
@@ -1,12 +1,19 @@
 package propofol.tilservice.domain.board.repository;
 
+import feign.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import propofol.tilservice.domain.board.entity.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query("select b from Board b")
+    @Transactional(readOnly = true)
     Page<Board> findAll(Pageable pageable);
+
+    @Query(countQuery = "select b from Board b where b.createdBy =: createdBy")
+    @Transactional(readOnly = true)
+    Page<Board> findPagesByCreatedBy(Pageable pageable, @Param("createdBy") String createdBy);
 }
