31f40a9f85e5283a3cdf7a3c9854b236335e13bc
최영찬 <gks54410@gmail.com>
2022-04-29 20:53:53 +0900

    Feat: 게시글에 저장된 이미지 가져오기

--- src/main/java/propofol/tilservice/domain/file/repository/ImageRepository.java
+++ src/main/java/propofol/tilservice/domain/file/repository/ImageRepository.java
@@ -1,8 +1,14 @@
 package propofol.tilservice.domain.file.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import propofol.tilservice.domain.file.entity.Image;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
+    @Query("select i from Image i where i.board.id=:boardId")
+    List<Image> findImages(@Param(value = "boardId") Long boardId);
 }
