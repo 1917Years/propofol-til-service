22c6f2f8399aabbf88aacd3e660706f1551ebcd1
최영찬 <gks54410@gmail.com>
2022-04-29 23:07:55 +0900

    Feat: 이름 수정

--- src/main/java/propofol/tilservice/api/controller/dto/BoardPageResponseDto.java
+++ src/main/java/propofol/tilservice/api/controller/dto/BoardPageResponseDto.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class BoardListResponseDto {
+public class BoardPageResponseDto {
     private Integer totalPageCount;
     private Long totalCount;
     private List<BoardResponseDto> boards = new ArrayList<>();
