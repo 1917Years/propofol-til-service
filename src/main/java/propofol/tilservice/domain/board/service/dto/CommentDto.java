fd98c976bebdfae05e7694bb404cff5eaeb31aae
최영찬 <gks54410@gmail.com>
2022-04-29 21:49:33 +0900

    Feat: 댓글 Dto 추가

---
+++ src/main/java/propofol/tilservice/domain/board/service/dto/CommentDto.java
@@ -0,0 +1,10 @@
+package propofol.tilservice.domain.board.service.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CommentDto {
+    private String content;
+}
