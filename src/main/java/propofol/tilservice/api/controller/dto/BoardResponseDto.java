03082a2c60016ccf1c5eba9723adb0af955c49b1
최영찬 <gks54410@gmail.com>
2022-04-30 19:01:04 +0900

    Feat: @AllArgsConstructor 추가

--- src/main/java/propofol/tilservice/api/controller/dto/BoardResponseDto.java
+++ src/main/java/propofol/tilservice/api/controller/dto/BoardResponseDto.java
@@ -1,10 +1,12 @@
 package propofol.tilservice.api.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class BoardResponseDto {
     private Long id;
     private String title;
