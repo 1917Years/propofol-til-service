6adcf403213abbf925fbf7dcb156e65f9b01124d
최영찬 <gks54410@gmail.com>
2022-04-30 13:47:26 +0900

    Feat: Comment 저장 기능 삭제

--- src/main/java/propofol/tilservice/domain/board/entity/Board.java
+++ src/main/java/propofol/tilservice/domain/board/entity/Board.java
@@ -52,11 +52,6 @@ public class Board extends BaseEntity {
         recommends.add(recommend);
     }
 
-    public void addComment(Comment comment) {
-        comment.addBoard(this);
-        comments.add(comment);
-    }
-
     public void setDownRecommend(){
         this.recommend = this.recommend - 1;
     }
