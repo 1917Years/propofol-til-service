2c2740dfa9f54fd597025c51c4a42e547c4dd8ec
최영찬 <gks54410@gmail.com>
2022-04-30 15:40:19 +0900

    Feat: 댓글 벌크 삭제 기능 추가

--- src/main/java/propofol/tilservice/domain/board/repository/CommentRepository.java
+++ src/main/java/propofol/tilservice/domain/board/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package propofol.tilservice.domain.board.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import propofol.tilservice.domain.board.entity.Comment;
@@ -11,4 +12,8 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c where c.board.id=:boardId group by c.groupId, c.id order by c.groupId, c.id asc")
     Page<Comment> findPageComments(@Param(value = "boardId") Long boardId, Pageable pageable);
+
+    @Modifying
+    @Query("delete from Comment c where c.board.id=:boardId")
+    int deleteBulkComments(@Param("boardId") Long boardId);
 }
