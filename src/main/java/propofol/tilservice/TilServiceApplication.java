3b073b0261b9bfd08ce194f8fae834f688e3d417
최영찬 <gks54410@gmail.com>
2022-05-14 02:13:36 +0900

    Feat: BCryptPasswordEncoder 삭제

--- src/main/java/propofol/tilservice/TilServiceApplication.java
+++ src/main/java/propofol/tilservice/TilServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -19,11 +18,6 @@ public class TilServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TilServiceApplication.class, args);
-	}
-
-	@Bean
-	public BCryptPasswordEncoder createEncoder(){
-		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
