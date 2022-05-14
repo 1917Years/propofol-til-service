1c2e763f65ab74d788ae93239895238247c3bf66
최영찬 <gks54410@gmail.com>
2022-05-14 22:14:11 +0900

    Feat: 이미지 조회 기능 추가

--- src/main/java/propofol/tilservice/domain/file/repository/ImageRepository.java
+++ src/main/java/propofol/tilservice/domain/file/repository/ImageRepository.java
@@ -1,12 +1,13 @@
 package propofol.tilservice.domain.file.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import propofol.tilservice.domain.file.entity.Image;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
@@ -18,4 +19,11 @@ public interface ImageRepository extends JpaRepository<Image, Long> {
 //    int deleteBulkImages(@Param(value = "boardId") Long boardId);w
 
     Image findImageByStoreFileName(String storeFileName);
+
+    @Query("select i from Image i where i.storeFileName in :names")
+    List<Image> findImagesInNames(@Param(value = "names") Collection<String> names);
+
+    Optional<Image> findTopByBoardId(Long boardId);
+
+    List<Image> findAllByBoardId(Long boardId);
 }
