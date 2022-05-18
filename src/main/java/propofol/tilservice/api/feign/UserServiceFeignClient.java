efacae4e5e0d6614700e72192281d59b072bb76a
최영찬 <gks54410@gmail.com>
2022-05-18 23:37:02 +0900

    Feat: 프로필 조회 기능 추가

--- src/main/java/propofol/tilservice/api/feign/UserServiceFeignClient.java
+++ src/main/java/propofol/tilservice/api/feign/UserServiceFeignClient.java
@@ -2,14 +2,11 @@ package propofol.tilservice.api.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-import propofol.tilservice.api.feign.dto.MemberInfoDto;
+import propofol.tilservice.api.controller.dto.ProfileImageResponseDto;
 import propofol.tilservice.api.feign.dto.StreakResponseDto;
 
 @FeignClient(name = "user-service")
 public interface UserServiceFeignClient {
-
-//    @GetMapping("/api/v1/members")
-//    MemberInfoDto getMemberInfo(@RequestHeader(name = "Authorization") String token);
 
     @PostMapping("/api/v1/members/streak")
     void saveStreak(@RequestHeader(name = "Authorization") String token,
@@ -18,5 +15,10 @@ public interface UserServiceFeignClient {
     @GetMapping("/api/v1/members/{memberId}")
     String getMemberNickName(@RequestHeader(name = "Authorization") String token,
                              @PathVariable("memberId") String memberId);
+
+    @PostMapping("/api/v1/members/commentProfile")
+    ProfileImageResponseDto getCommentProfile(
+            @RequestHeader(name="Authorization") String token,
+            @RequestBody String nickname);
 
 }
