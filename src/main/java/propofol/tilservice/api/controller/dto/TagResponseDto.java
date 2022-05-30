83c6382bc4b9d89c280dd13097af8656b5b8c12c
최영찬 <gks54410@gmail.com>
2022-05-30 20:20:13 +0900

    Feat: TagResponseDto 수정

--- src/main/java/propofol/tilservice/api/controller/dto/TagResponseDto.java
+++ src/main/java/propofol/tilservice/api/controller/dto/TagResponseDto.java
@@ -1,8 +1,12 @@
 package propofol.tilservice.api.controller.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TagResponseDto {
     private Long id;
     private String name;
