8a9ec28b4e2a925823d3ba5bd01ca73cc84a2c5e
최영찬 <gks54410@gmail.com>
2022-06-25 14:33:05 +0900

    Feat: boardId 전송 추가

--- src/main/java/propofol/tilservice/api/feign/service/AlarmService.java
+++ src/main/java/propofol/tilservice/api/feign/service/AlarmService.java
@@ -15,13 +15,12 @@ public class AlarmService {
 
     private final AlarmServiceFeignClient alarmServiceFeignClient;
 
-    public void saveAlarm(long toId, String message, String token, AlarmType type){
-        alarmServiceFeignClient.saveAlarm(token, new AlarmSaveDto(toId, message, type, null));
+    public void saveAlarm(long toId, String message, String token, AlarmType type, Long boardId){
+        alarmServiceFeignClient.saveAlarm(token, new AlarmSaveDto(toId, message, type, boardId));
     }
 
     public void saveListAlarm(List<Long> toIds, String message, String type, Long boardId, String token){
         AlarmSaveListDto alarmSaveListDto = new AlarmSaveListDto(toIds, message, type, boardId);
-        System.out.println("alarmSaveListDto.getMessage() = " + alarmSaveListDto.getMessage());
         alarmServiceFeignClient.saveListAlarm(token, alarmSaveListDto);
     }
 }
